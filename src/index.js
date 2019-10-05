124c1b18f29a62058a3b5dafc21b9d7bb958271f
Scott Davis <stdavis@utah.gov>
2019-10-04 22:38:20 -0600

    feat: add agrc eslint config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command');
 
 class GoodSamaritanCommand extends Command {
   async run() {
-    const {flags} = this.parse(GoodSamaritanCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.js`)
+    const name = flags.name || 'world';
+    this.log(`hello ${name} from ./src/index.js`);
   }
 }
 
 GoodSamaritanCommand.description = `Describe the command here
 ...
 Extra documentation goes here
-`
+`;
 
 GoodSamaritanCommand.flags = {
   // add --version flag to show CLI version
-  version: flags.version({char: 'v'}),
+  version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
-  help: flags.help({char: 'h'}),
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
+  help: flags.help({ char: 'h' })
+};
 
-module.exports = GoodSamaritanCommand
+module.exports = GoodSamaritanCommand;
