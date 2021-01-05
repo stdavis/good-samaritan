f1d0397ec44057a5657804777b5d4651a1fffefd
stdavis <stdavis@utah.gov>
2021-01-05 15:50:46 -0700

    feat: search sub-dependencies

--- src/packages.js
+++ src/packages.js
@@ -5,9 +5,34 @@ const packageInfo = require('package-json');
 const getCurrentProjectDependencies = async () => {
   const packageJson = await readPackage();
 
-  return {
+  let dependencies = {
     ...packageJson.dependencies,
     ...packageJson.devDependencies
+  };
+
+  for (let packageName in dependencies) {
+    const subPackageDependencies = await getDependencies(packageName, dependencies[packageName]);
+
+    dependencies = {
+      ...subPackageDependencies,
+      // preference for root-level dependencies
+      ...dependencies
+    };
+  }
+
+  return dependencies;
+};
+
+const getDependencies = async (packageName, version) => {
+  // TODO: cache
+  const info = await packageInfo(packageName, {
+    version: version,
+    fullMetadata: true
+  });
+
+  return {
+    ...info.dependencies,
+    ...info.devDependencies
   };
 };
 
@@ -19,7 +44,6 @@ const getRepoUrl = async (packageName, version) => {
 
   return (info.repository && info.repository.url) ? info.repository.url : null;
 };
-
 
 module.exports = {
   getCurrentProjectDependencies,
